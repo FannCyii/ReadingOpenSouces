3fe9dfed19a4e0e13b683ceee58a62d1522377ec
Kivan <cqf123>
2018-02-04 00:58:12 +0800

    add AFN

---
+++ AFNetworkingNode/Demo_AFN/Demo_AFN/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo_AFN
+//
+//  Created by chengqifan on 2017/2/26.
+//  Copyright © 2017年 yhd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
